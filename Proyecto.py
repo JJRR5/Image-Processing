1ac56c7265aa1a1ca4639fd9e3273461ff9b171b
Jose Rodriguez <76703666+JJRR5@users.noreply.github.com>
2021-10-13 07:41:52 -0500

    ACOMODO DE IMAGENES
    Se acomodaron las imagenes, y se agrego color de fondo y se modifico el cursor

--- Proyecto.py
+++ Proyecto.py
@@ -9,10 +9,12 @@ ventana.title("Histograma y ecualización")
 
 miframe=tk.Frame(ventana)
 miframe.pack()
+miframe.config(bg="light sky blue",cursor='hand2')
 
 mensaje=tk.Label(miframe,text="Elige una de las 3 imagenes:",font=('Arial',25))
-mensaje.place()
-mensaje.pack()
+'''mensaje.place()
+mensaje.pack()'''
+mensaje.grid(row=0,column=1,padx=10,pady=10)
 
 img1=Image.open('paisaje.jpg')
 img1=img1.resize((250, 250),Image.ANTIALIAS)
@@ -23,14 +25,14 @@ img2=ImageTk.PhotoImage(img2)
 img3=Image.open('Chito.png')
 img3=img3.resize((250, 250),Image.ANTIALIAS)
 img3=ImageTk.PhotoImage(img3)
+
 boton1=tk.Button(miframe,image=img1)
-boton1.place()
-boton1.pack()
+boton1.grid(row=1,column=0,padx=5,pady=10)
+
 boton2=tk.Button(miframe,image=img2)
-boton2.place()
-boton2.pack()
+boton2.grid(row=1,column=1,padx=5,pady=10)
+
 boton3=tk.Button(miframe,image=img3)
-boton3.place()
-boton3.pack()
+boton3.grid(row=1,column=2,padx=5,pady=10)
 
 ventana.mainloop()
